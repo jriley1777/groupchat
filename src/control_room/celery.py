67dbd75f68e96cf269706d0bae374244f5c76b8d
Joseph Riley <jriley@pandora.com>
2017-09-30 14:37:06 -0700

    add celery module

--- src/control_room/celery.py
+++ src/control_room/celery.py
@@ -4,11 +4,11 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groupchat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_room.settings')
 
 from django.conf import settings  # noqa
 
-app = Celery('groupchat')
+app = Celery('control_room')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
