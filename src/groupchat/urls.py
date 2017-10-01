95e4d64f2338d1d01f95d13983e2c581cb24ebbe
Joseph Riley <jriley@pandora.com>
2017-09-30 21:55:17 -0700

    add ability to switch rooms, list messages, add messages

--- src/groupchat/urls.py
+++ src/groupchat/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^room/list$', views.list_rooms, name='list_rooms'),
+    url(r'^room/(?P<room_id>[0-9]+)/get_messages/$', views.get_room_messages, name='get_room_messages'),
+    url(r'^room/(?P<room_id>[0-9]+)/message/add$', views.add_room_message, name='add_room_message'),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- src/groupchat/views/base_views.py
+++ src/groupchat/views/base_views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from groupchat import tasks
+from groupchat.models import Room, RoomMessage, User
+from django.http import JsonResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 def index(request):
 	tasks.print_hello.delay()
 	return render(request,'index.html')
+
+def list_rooms(request):
+	rooms = Room.objects.values('id','name').all().order_by('id')
+	data = list(rooms)
+	return JsonResponse(data,safe=False)
+
+def get_room_messages(request, room_id):
+	messages = RoomMessage.objects.values('message_time','user__username','content').filter(room=room_id).order_by('message_time')
+	data = list(messages)
+	return JsonResponse(data,safe=False)
+
+def add_room_message(request,room_id):
+	message = RoomMessage()
+	message.user = User.objects.get(pk=1)
+	message.room = Room.objects.get(pk=room_id)
+	message.content = request.GET.get('msg')
+	message.save()
+	return HttpResponseRedirect(reverse('get_room_messages',args=(room_id,)))
