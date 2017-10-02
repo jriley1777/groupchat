75e57a1cab7597b8b39fcdc73fe8f6691da36378
Joseph Riley <jriley@pandora.com>
2017-10-02 00:00:03 -0700

    new commands to shell; create and delete rooms from shell;

--- src/groupchat/urls.py
+++ src/groupchat/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^friend/list$', views.list_friends, name='list_friends'),
-    url(r'^room/list$', views.list_rooms, name='list_rooms'),
+    url(r'^friend/list/$', views.list_friends, name='list_friends'),
+    url(r'^room/list/$', views.list_rooms, name='list_rooms'),
+    url(r'^room/create/$', views.create_room, name='create_room'),
+    url(r'^room/delete/$', views.delete_room, name='delete_room'),
     url(r'^room/(?P<room_id>[0-9]+)/get_messages/$', views.get_room_messages, name='get_room_messages'),
-    url(r'^room/(?P<room_id>[0-9]+)/message/add$', views.add_room_message, name='add_room_message'),
+    url(r'^room/(?P<room_id>[0-9]+)/message/add/$', views.add_room_message, name='add_room_message'),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- src/groupchat/views/base_views.py
+++ src/groupchat/views/base_views.py
@@ -3,6 +3,7 @@ from groupchat import tasks
 from groupchat.models import Room, RoomMessage, User
 from django.http import JsonResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,9 +21,25 @@ def list_friends(request):
 	data = list(user)
 	return JsonResponse(data,safe=False)
 
+def create_room(request):
+	room = Room()
+	room.name = request.GET.get('room_name')
+	room.creator = User.objects.get(pk=1)
+	room.save()
+	return render(request, 'index.html')
+
+def delete_room(request):
+	room_name = request.GET.get('room_name')
+	room = Room.objects.get(name=room_name)
+	if room.creator == request.user:
+		room.delete()
+	else:
+		messages.info(request,"You are not the owner of this room and cannot delete.")
+	return HttpResponseRedirect(reverse('index'))
+
 def get_room_messages(request, room_id):
 	messages = RoomMessage.objects.values('message_time','user__username','content').filter(room=room_id).order_by('message_time')
-	data = list(messages)
+	data = list(messages)[-15:]
 	return JsonResponse(data,safe=False)
 
 def add_room_message(request,room_id):
